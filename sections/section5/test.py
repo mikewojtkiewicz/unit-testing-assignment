c3c8158d8cd274eafe487a3d37a205412059f281
Mike Wojtkiewicz <mwojtkiewicz@origamirisk.com>
2025-10-03 09:45:40 -0400

    Initial commit

---
+++ sections/section5/test.py
@@ -0,0 +1,18 @@
+import unittest
+from code.my_calculations import Calculations
+
+class TestCalculations(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculations(8, 2)
+
+    def test_sum(self):
+        self.assertEqual(self.calc.get_sum(), 10)
+
+    def test_diff(self):
+        self.assertEqual(self.calc.get_difference(), 6)
+
+    def test_product(self):
+        self.assertEqual(self.calc.get_product(), 16)
+
+    def test_quotient(self):
+        self.assertEqual(self.calc.get_quotient(), 4)
